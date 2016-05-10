3e38d4e66153e6f5e4e3299848a88ebe25fdea04
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2016-05-10 09:26:30 +0530

    Added get_authorizations

--- libgist/auth.py
+++ libgist/auth.py
@@ -30,8 +30,10 @@ def generate_new_token(username, password, _otp=None, _url=None):
     '''
     Create a new authorization.
     '''
+    url = 'https://api.github.com/authorizations'
     payload = {
-        'note': 'gist-shell-token',
+        'note': 'gist-shell',
+        'note_url': 'https://github.com/clickyotomy/gist-shell',
         'scopes': ['gist'],
         'fingerprint': generate_fingerprint()
     }
@@ -44,9 +46,38 @@ def generate_new_token(username, password, _otp=None, _url=None):
 
     if _url is not None:
         url = '/'.join([_url, 'authorizations'])
-    else:
-        url = 'https://api.github.com/authorizations'
 
     response = requests.post(url, headers=headers, data=json.dumps(payload),
                              auth=(username, password))
-    return response
+
+    return response.json()
+
+
+def get_authorizations(username, password, _otp=None, _url=None, _id=None):
+    '''
+    Get all the authorizations created using libgist (note: gist-shell).
+    '''
+    authorizations = list()
+    url = 'https://api.github.com/authorizations'
+    headers = headers = {
+        'Accept': 'application/vnd.github.damage-preview+json'
+    }
+
+    if _otp is not None:
+        headers.update({'X-GitHub-OTP': _otp})
+
+    if _url is not None:
+        url = '/'.join([_url, 'authorizations'])
+
+    if _id is not None:
+        url = '/'.join([url, _id])
+
+    response = requests.get(url, auth=(username, password), headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        for authorization in data:
+            if str(authorization['note']) == 'gist-shell':
+                authorizations.append(authorization)
+
+    return authorizations
