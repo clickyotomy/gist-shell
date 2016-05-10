bf6d10b7abd35bccf38381e3a3b9179026b66c48
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2016-05-10 17:08:29 +0530

    Added delete_authorization

--- libgist/auth.py
+++ libgist/auth.py
@@ -3,6 +3,12 @@
 '''
 Authentication modules for GitHub OAuth2 (not the web flow).
 Perform CRUD operations on access tokens; authenticate users.
+Common arguments:
+    # username: GitHub username.
+    # password: GitHub password.
+    # _otp: One Time Password (X- GitHub-OTP) if 2fa
+      (two factor authentication) is enabled.
+    # _url: Custom API endpoint.
 '''
 
 import json
@@ -26,7 +32,7 @@ def generate_fingerprint():
     return fingerprint
 
 
-def generate_new_token(username, password, _otp=None, _url=None):
+def create_authorization(username, password, _otp=None, _url=None):
     '''
     Create a new authorization.
     '''
@@ -53,9 +59,10 @@ def generate_new_token(username, password, _otp=None, _url=None):
     return response.json()
 
 
-def get_authorizations(username, password, _otp=None, _url=None, _id=None):
+def get_authorization(username, password, _id=None, _otp=None, _url=None):
     '''
     Get all the authorizations created using libgist (note: gist-shell).
+    Lists a specific authorization if the '_id' argument is passed.
     '''
     authorizations = list()
     url = 'https://api.github.com/authorizations'
@@ -76,8 +83,44 @@ def get_authorizations(username, password, _otp=None, _url=None, _id=None):
 
     if response.status_code == 200:
         data = response.json()
-        for authorization in data:
-            if str(authorization['note']) == 'gist-shell':
-                authorizations.append(authorization)
+        if _id is not None:
+            authorizations.append(data)
+        else:
+            for authorization in data:
+                if str(authorization['note']) == 'gist-shell':
+                    authorizations.append(authorization)
 
     return authorizations
+
+
+def delete_authorization(username, password, _id, _otp=None, _url=None):
+    '''
+    Update an authorization (to generate a new access token).
+    '''
+    authorizations = []
+    url = 'https://api.github.com/authorizations'
+    headers = headers = {
+        'Accept': 'application/vnd.github.damage-preview+json'
+    }
+
+    if _otp is not None:
+        headers.update({'X-GitHub-OTP': _otp})
+
+    if _url is not None:
+        url = '/'.join([_url, 'authorizations'])
+
+    if _id is not None:
+        url = '/'.join([url, _id])
+        authorizations = get_authorization(username, password)
+
+    if len(authorizations) > 0:
+        responses = list()
+        for auth in authorizations:
+            url = '/'.join([url, auth['id']])
+            response = requests.delete(url, headers=headers,
+                                       auth=(username, password))
+            responses.append(response.json())
+    else:
+        response = requests.delete(url, headers=headers,
+                                   auth=(username, password))
+        return response.json()
